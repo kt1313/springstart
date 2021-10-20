7f325866cc650e8547a968b74248c57e2e7263c1
kt1313 <tomaklim@wp.pl>
2021-10-20 07:08:32 +0200

    Initial commit

---
+++ src/test/java/pl/clockworkjava/springstart/SpringstartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.clockworkjava.springstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
