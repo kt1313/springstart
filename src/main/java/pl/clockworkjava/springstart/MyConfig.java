1e5d584c9c6aaf4f775e9660c0109c565039c0b5
kt1313 <tomaklim@wp.pl>
2021-10-22 07:43:06 +0200

    5. 1_10 wstrzykiwanie listy beanow;  wstrzykiwanie wybranych beanow sposrod utworzonych w MyConfig

--- src/main/java/pl/clockworkjava/springstart/MyConfig.java
+++ src/main/java/pl/clockworkjava/springstart/MyConfig.java
@@ -8,23 +8,34 @@ import pl.clockworkjava.springstart.di.Weather;
 import pl.clockworkjava.springstart.di.Window;
 
 import javax.net.ssl.HostnameVerifier;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class MyConfig {
 
     @Bean
-    public Window window1(@Qualifier("sunny") Weather weather){
-        return new Window( weather);
+    public Window window1(@Qualifier("sunny") Weather weather) {
+        return new Window(weather);
     }
 
     @Bean
-    public Window window2( @Qualifier("rainy") Weather weather){
-
+    public Window window2(@Qualifier("rainy") Weather weather) {
         return new Window(weather);
     }
+
+    @Bean
+    public Window window3(@Qualifier("rainy") Weather weather) {
+        return new Window(weather);
+    }
+
     @Bean("rainy")
-    public Weather weather(){
+    public Weather weather() {
         return new Weather();
+    }
+@Bean
+    public List<Window> myWindows(@Qualifier("sunny")Weather weather){
+        return Arrays.asList(window1(weather), window2(weather));
     }
 //@Bean
 //    public House house(){
--- src/main/java/pl/clockworkjava/springstart/di/House.java
+++ src/main/java/pl/clockworkjava/springstart/di/House.java
@@ -1,20 +1,29 @@
 package pl.clockworkjava.springstart.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.swing.plaf.PanelUI;
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class House {
 
     List<Window> windows;
 
+    public House(){
+        System.out.println("[HOUSE]Konstruktor");
+    }
+
     @Autowired
-public House(List<Window> windows){
+    @Qualifier("myWindows")
+  public void setWindows(List<Window> windows){
+        System.out.println("[HOUSE]metoda ");
         this.windows=windows;
     }
+
     public void peek() {
         windows.forEach(Window::peek);
     }
