e581ea81763c84268f6a56aa5af9a8c7b22b9fe9
kt1313 <tomaklim@wp.pl>
2021-10-28 10:16:29 +0200

    7. 1_13 testy z kontekstem

--- src/test/java/pl/clockworkjava/springstart/SpringstartApplicationTests.java
+++ src/test/java/pl/clockworkjava/springstart/SpringstartApplicationTests.java
@@ -1,10 +1,27 @@
 package pl.clockworkjava.springstart;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.clockworkjava.springstart.di.Weather;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringstartApplicationTests {
+
+	@Autowired
+	@Qualifier("sunny")Weather weather;
+
+	@Test
+	public void weatherInjectionTest(){
+		Assert.assertFalse(weather.description.isEmpty());
+	}
+
+
 
 	@Test
 	void contextLoads() {
